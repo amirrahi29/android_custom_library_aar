dc9306b3d71b1f07b38d94da11e5d0c175ce96cc
Amir Rahi <amirrahi29@gmail.com>
2023-05-31 16:17:28 +0530

    added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.mycustomlibrary.MyCustomProject;
 
@@ -25,17 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void addNumber(View view) {
         Log.e("added value: ", String.valueOf(myCustomProject.addTwoNumbers(3, 9)));
+        Toast.makeText(this, String.valueOf(myCustomProject.addTwoNumbers(3, 9)), Toast.LENGTH_LONG).show();
     }
 
     public void minusClick(View view) {
         Log.e("minus value: ", String.valueOf(myCustomProject.minusTwoNumbers(20, 9)));
+        Toast.makeText(this, String.valueOf(myCustomProject.minusTwoNumbers(20, 9)), Toast.LENGTH_LONG).show();
     }
 
     public void multipleNumber(View view) {
         Log.e("multiplied value: ", String.valueOf(myCustomProject.multiplyTwoNumbers(5, 5)));
+        Toast.makeText(this, String.valueOf(myCustomProject.multiplyTwoNumbers(5, 5)), Toast.LENGTH_LONG).show();
     }
 
     public void divideNumber(View view) {
         Log.e("divided value: ", String.valueOf(myCustomProject.divideTwoNumbers(30, 5)));
+        Toast.makeText(this, String.valueOf(myCustomProject.divideTwoNumbers(30, 5)), Toast.LENGTH_LONG).show();
     }
 }
